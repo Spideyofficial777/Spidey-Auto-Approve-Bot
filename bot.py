347accb950e910eefb319036dd8776c3ff84109f
dy3no <165803560+dy3no@users.noreply.github.com>
2024-12-21 15:09:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from pyrogram import Client, filters
 from pyrogram.types import *
-from motor.motor_asyncio import AsyncIOMotorClient  
+from motor.motor_asyncio import  
 from os import environ as env
 import asyncio, datetime, time
 
